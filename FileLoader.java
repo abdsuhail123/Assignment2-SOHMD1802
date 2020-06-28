422f51dad085dc96cdd29bb4486451a4ace9d412
Abdullah Sohail <Muhammad Abdullah Sohail>
2020-06-28 07:24:50 +0500

    Phase 4 Complete
    Added loadHero method to FileLoader.class

--- FileLoader.java
+++ FileLoader.java
@@ -1,20 +1,39 @@
 import java.io.*; // Import Files and Exceptions
 import java.util.Scanner; // Import the Scanner class
 
-// This class reads in a .txt file
-// The first line of the file contains row and col of the Map
-// Then the map template is present which will be copied to our new map object
+/* The FileLoader class reads in data from .txt files for our code */     
 
 public class FileLoader
 {
-    //private static String mapFile = "mapFile.txt" ;
+    //=========================== For loadHero Method ============================
+
+    private static Hero newHero = new Hero(); //New Hero Object
+    private static String name; // Variable for Hero Name
+    private static String description; // Variable for Hero Description
+    private static int stress; // Variable for Hero Stress
+    private static int maxStress; // Variable for Maximum Stress
+    private static int physical; // Variable for Physical Skill
+    private static int mental; // Variable for Mental Skill
+    private static int social; // Variable for Social Skill
+    private static Item newItem ; // Variable for new Item
+
+    //=========================== For loadMap  Method ============================
+
+    private static char mapTemplate[][] ; //2D array of characters
+    private static Map newMap = new Map(10,10); //Initiliazing our new map object
     private static int row ; // row variable
     private static int col ; // column variable
+    //private static String mapFile = "mapFile.txt" ;
+
+    //=============================================================================
+
+    /* loadMap Method */
+
+    // This method reads in a "mapFile.txt"
+    // The first line of the file contains row and col of the Map
+    // Then the map template is present which will be copied to our new map object
 
     public static Map loadMap(String fileName){
-        char mapTemplate[][] ; //2D array of characters
-        Map newMap = new Map(10,10); //Initiliazing our new map object
-
         // Try catch
         try{
             // Load the file without errors
@@ -31,8 +50,6 @@ public class FileLoader
                 //Error message
                 System.out.println("Error, No data in saved text file");
             }
-
-            //System.out.println(line);
 
             // Scanner parses the line
             Scanner scan = new Scanner(line);
@@ -52,8 +69,10 @@ public class FileLoader
                     token = buffer.read(); //Reading in token value as int
                     index = (char)token ; //casting token value as char in index
                     mapTemplate[i][j] = index; //placing that index
+                    System.out.print(mapTemplate[i][j]);
                 }
                 line = buffer.readLine(); //reset to the next line
+                System.out.println();
             }
 
             //Creating a new Map with our template
@@ -75,5 +94,86 @@ public class FileLoader
             exc.printStackTrace();
         }
         return newMap;
+    }
+
+    /* loadHero Method */
+
+    // This method reads in the "heroFile.txt"
+    // The first line has the name, the second line has the description
+    // The third line has the Integer values for stress and maxstress
+    // The fourth line has the Integer values for skills (Physical,Mental,Social)
+    // The fifth line has ONE item in the Hero inventory
+
+    public static Hero loadHero(String heroFile){
+
+        // Try Catch
+        try{
+            // Load the file without errors
+            FileReader reader = new FileReader(heroFile);
+
+            // BufferedReader will read in our file
+            BufferedReader buffer = new BufferedReader(reader);
+
+            // Get the 1st line from the BufferedReader
+            String line = buffer.readLine();
+            //If there is no first line
+            if(line==null){
+                //Error message
+                System.out.println("Error, No data in saved text file");
+            }
+            name = line; // 1st line is the name in the format
+
+            // Get the 2nd line from the BufferedReader
+            line = buffer.readLine();
+            description = line ; // 2nd line is the description in the format
+
+            // Get the 3rd line from the BufferedReader
+            line = buffer.readLine(); 
+
+            // New Scanner to parse the lines
+            Scanner scan = new Scanner(line);
+
+            stress = scan.nextInt(); // 1st integer in 3rd line = stress
+            maxStress = scan.nextInt(); // 2nd integer in 3rd line = maxStress
+
+            // Get the 4th line from the BufferedReader
+            line = buffer.readLine();
+
+            // Using scanner again
+            scan = new Scanner(line);
+
+            physical = scan.nextInt(); // 1st integer in 4th line = physicalSkill
+            mental = scan.nextInt(); // 2nd integer in 4th line = mentalSkill
+            social = scan.nextInt(); // 3rd integer in 4th line = socialSkill
+
+            // Getting the fifth line (Contains ONE random item)
+            line = buffer.readLine();
+            newItem = new Item(line); // Reading name of item
+            newHero.addItem(newItem); // Adding item to Hero inventory
+
+            // Creating new Hero object.
+            // Name, Description, Stress, MaxStress, Physical, Mental and Social
+            // All are read from the .txt file
+
+            newHero = new Hero(name,description,stress,maxStress);
+            newHero.setPhysical(physical);
+            newHero.setMental(mental);
+            newHero.setSocial(social);
+
+            //System.out.println(newHero.fullToString());
+
+        }catch(IOException exc){ // Catch block for IOException
+            //Error message
+            System.out.println("Error reading in file/ IO Exception");
+            //Error Trace
+            exc.printStackTrace();
+
+        }catch(Exception exc){ // Catch block for random exception
+            //Error message
+            System.out.println("Unknown Exception");
+            //Error Trace
+            exc.printStackTrace();
+        }
+        return newHero; // Return the newHero
     }
 }
--- Hero.java
+++ Hero.java
@@ -2,163 +2,176 @@
 // only ever be one instance of each Hero at a time. 
 public class Hero{
 
-	// Phase 1
-	private String name; // Name of the Hero
-	private String description; // Description of the Hero. 
+    // Phase 1
+    private String name; // Name of the Hero
+    private String description; // Description of the Hero. 
 
-	private int currentStress;
-	private int stressLimit; // reach this and you are done. Pass out. 
-	private boolean isAwake = true; // if hp = 0, pass out. 
+    private int currentStress;
+    private int stressLimit; // reach this and you are done. Pass out. 
+    private boolean isAwake = true; // if hp = 0, pass out. 
 
-	// Stats - min 1, max 5
-	// make up your own skills if you want
+    // Stats - min 1, max 5
+    // make up your own skills if you want
 
-	private int physicalSkill; // strength, agility, constitution
-	private int mentalSkill; // intelligence, wisdom, mental sharpness
-	private int socialSkill; // charisma, empathy and other group skills
+    private int physicalSkill; // strength, agility, constitution
+    private int mentalSkill; // intelligence, wisdom, mental sharpness
+    private int socialSkill; // charisma, empathy and other group skills
 
-//	private String[] traits; // Like tags, can have other abilities. 
+    //	private String[] traits; // Like tags, can have other abilities. 
 
-	private ItemList inventory; // All Items collected so far
+    private ItemList inventory; // All Items collected so far
 
-	// ******* Initialization *******
-	public Hero(){
-		name = "Default";
-		description = "None Given";
-		currentStress = 0;
-		stressLimit = 10;
-		inventory = new ItemList();
+    // ******* Initialization *******
+    public Hero(){
+        name = "Default";
+        description = "None Given";
+        currentStress = 0;
+        stressLimit = 10;
+        inventory = new ItemList();
 
-	}
-	// basic constructor.
-	public Hero(String newName, String newDescription, int currentStress, int stressLimit){
-		name = newName;
-		description = newDescription;
+    }
+    // basic constructor.
+    public Hero(String newName, String newDescription, int currentStress, int stressLimit){
+        name = newName;
+        description = newDescription;
 
-		// stress
-		this.currentStress = currentStress;
-		this.stressLimit = stressLimit;
+        // stress
+        this.currentStress = currentStress;
+        this.stressLimit = stressLimit;
 
-		// set up items. 
-		inventory = new ItemList();
-	}
+        // set up items. 
+        inventory = new ItemList();
+    }
 
-	// Option B1:
-	// Could also be done in the constructor if you wanted. 
-	public void setSkills(int newPhysical, int newMental, int newSocial){
-		physicalSkill = newPhysical; // strength, agility, constitution
-		mentalSkill = newMental; // intelligence, wisdom, mental sharpness
-		socialSkill = newSocial;
-	}
+    // Option B1:
+    // Could also be done in the constructor if you wanted. 
+    public void setSkills(int newPhysical, int newMental, int newSocial){
+        physicalSkill = newPhysical; // strength, agility, constitution
+        mentalSkill = newMental; // intelligence, wisdom, mental sharpness
+        socialSkill = newSocial;
+    }
 
-	// Return only the name for now. 
-	public String toString(){
-		return name;
-	}
+    // Return only the name for now. 
+    public String toString(){
+        return name;
+    }
 
-	// p1
-	// Return the name
-	public String getName(){
-		return name;
-	}
+    // p1
+    // Return the name
+    public String getName(){
+        return name;
+    }
 
-	// "Accessor" method for description
-	public String getDescription(){
-		return description;
-	}
+    // "Accessor" method for description
+    public String getDescription(){
+        return description;
+    }
 
-	// include description and skills as well. 
-	public String profileToString(){
-		return name + "\n" + description 
-		+ "\n - Stress: " + currentStress + "/" + stressLimit;
-	}
-	// Return the skills only. 
-	public String skillsToString(){
-		return " - physical: " + physicalSkill
-		+ "\n - mental:" + mentalSkill
-		+ "\n - social:" + socialSkill;
-	}
+    // include description and skills as well. 
+    public String profileToString(){
+        return name + "\n" + description 
+        + "\n - Stress: " + currentStress + "/" + stressLimit;
+    }
+    // Return the skills only. 
+    public String skillsToString(){
+        return " - physical: " + physicalSkill
+        + "\n - mental:" + mentalSkill
+        + "\n - social:" + socialSkill;
+    }
 
-	// P3
-	// Items
-	public void addItem(Item newItem){
-		inventory.addItem(newItem);
-	}
+    // P3
+    // Items
+    
+    
+    public void addItem(Item newItem){
+        inventory.addItem(newItem);
+    }
 
-	public String allItems(){
-		return inventory.toString();
-	}
-	
-	// Name, Description, all Skills and All Items. 
-	public String fullToString(){
-		return profileToString() + "\n" + skillsToString() + "\n" + allItems();
-	}
+    public String allItems(){
+        return inventory.toString();
+    }
 
+    // Name, Description, all Skills and All Items. 
+    public String fullToString(){
+        return profileToString() + "\n" + skillsToString() + "\n" + allItems();
+    }
 
-	// Alternate way of assigning variables, using "this" keyword
-	// "Mutator" method for description
-	// For now I am overwriting the old description. This could be improved
-	// by allowing for appending. 
-	public void setDescription(String description){
-		this.description = description;
-	}
+    // Alternate way of assigning variables, using "this" keyword
+    // "Mutator" method for description
+    // For now I am overwriting the old description. This could be improved
+    // by allowing for appending. 
+    public void setDescription(String description){
+        this.description = description;
+    }
 
-	// Phase 5 - Still needs doing. 
-	// Checks a skill against the stat of the Hero.
-	// If the value is less then the given skill plus a random value between 1-4. return true 
-	// Use strings for now to check type
-	// "mental", "physical", "social"
-	// True means you passed. 
-	public boolean skillCheck(String skillType, int value){
-		boolean outcome = false;
-		int baseRoll = RandomGenerator.randomRoll(1,4);
-		System.out.println(" - DEBUG: RANDOM ROLL: "+ baseRoll);
+    // Phase 5 - Still needs doing. 
+    // Checks a skill against the stat of the Hero.
+    // If the value is less then the given skill plus a random value between 1-4. return true 
+    // Use strings for now to check type
+    // "mental", "physical", "social"
+    // True means you passed. 
+    public boolean skillCheck(String skillType, int value){
+        boolean outcome = false;
+        int baseRoll = RandomGenerator.randomRoll(1,4);
+        System.out.println(" - DEBUG: RANDOM ROLL: "+ baseRoll);
 
-		if(skillType.equals("mental")){
-			if(value <= mentalSkill + baseRoll){
-				outcome = true;
-			}
-		}else if(skillType.equals("physical")){
-			if(value <= physicalSkill + baseRoll){
-				System.out.println(" - DEBUG: physicalSkill + baseRoll " + physicalSkill + ' ' + baseRoll);
-				outcome = true;
-			}
-		}else if(skillType.equals("social")){
-			if(value <= socialSkill + baseRoll){
-				outcome = true;
-			}
-		}else{
-			// a character with no skill should check with level 0. 
-			// helps with bugs, should not be called. 
-			System.out.println("Warning: Unknown skill found: " + skillType);
-		}
-		return outcome;
-	}
+        if(skillType.equals("mental")){
+            if(value <= mentalSkill + baseRoll){
+                outcome = true;
+            }
+        }else if(skillType.equals("physical")){
+            if(value <= physicalSkill + baseRoll){
+                System.out.println(" - DEBUG: physicalSkill + baseRoll " + physicalSkill + ' ' + baseRoll);
+                outcome = true;
+            }
+        }else if(skillType.equals("social")){
+            if(value <= socialSkill + baseRoll){
+                outcome = true;
+            }
+        }else{
+            // a character with no skill should check with level 0. 
+            // helps with bugs, should not be called. 
+            System.out.println("Warning: Unknown skill found: " + skillType);
+        }
+        return outcome;
+    }
 
-	// add negative to remove it
-	// this is kind of problematic. 
-	public void addStress(int amount){		
-		currentStress = currentStress + amount; 
-	}
+    // add negative to remove it
+    // this is kind of problematic. 
+    public void addStress(int amount){		
+        currentStress = currentStress + amount; 
+    }
 
-	// Status of true means things are ok to go on
-	// status of false means they have passed out. 
-	public boolean checkStatus(){
-		boolean returnVal = false;
-		if(isAwake){
-			
-			if(currentStress < stressLimit){
-				// Things are OK 
-				returnVal = true;
-			}else{
-				// trigger the pass out. 
-				isAwake = false;
-			}
+    // Status of true means things are ok to go on
+    // status of false means they have passed out. 
+    public boolean checkStatus(){
+        boolean returnVal = false;
+        if(isAwake){
 
-		}else{
-			// no way to wake up yet
-			
-		}
-		return returnVal;
-	}
+            if(currentStress < stressLimit){
+                // Things are OK 
+                returnVal = true;
+            }else{
+                // trigger the pass out. 
+                isAwake = false;
+            }
+
+        }else{
+            // no way to wake up yet
+
+        }
+        return returnVal;
+    }
+
+    public void setPhysical(int p){
+        physicalSkill = p;
+    }
+
+    public void setMental(int m){
+        mentalSkill = m;
+    }
+
+    public void setSocial(int s){
+        socialSkill = s;
+    }
 }
